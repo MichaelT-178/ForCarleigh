9e26831fc6af79448dd0da6d2eaed24a10d5415f
Michael Totaro <mtotaro@ncsu.edu>
2022-10-30 14:13:38 -0400

    Adding files

---
+++ date.py
@@ -0,0 +1,35 @@
+from datetime import date, datetime
+
+t = str(date.today()).split("-")
+month = t[1].replace("0","") if (t[1][0] == "0") else t[1]
+
+todays_date = month + '/' + t[2] + "/" + t[0]
+end_date = "11/22/2022"
+
+class DateException(Exception):
+    pass
+
+def check_date(date1):
+    s = [int(x) for x in date1.split("/")]
+    
+    if (len(s) != 3) or (s[0] > 12) or (s[1] > 31) or (s[2] < 0):
+        raise DateException("Date formatted incorrectly!")
+
+    if (s[2] > 9 and s[2] < 100):
+        s[2] = "20" + s[2]
+
+    return s
+
+def convert_to_min(d):
+    l = [int(x) for x in d.split(":")]
+    hours, mins, secs = 23 - l[0], 60 - l[1], 60 - l[2]
+    return f"{hours} Hours {mins} Minutes {secs} Seconds left today!\n"
+
+if (len(todays_date.split("/")) == 3):
+    m1, d1, y1 = check_date(todays_date)
+    m2, d2, y2 = check_date(end_date)
+    days_left = (date(y2, m2, d2) - date(y1, m1, d1)).days
+    print(f"\n{days_left} days left until Thanksgiving break!")
+    print(f"{days_left/7:.2f} weeks left!")
+
+print(convert_to_min(datetime.now().strftime("%H:%M:%S")))
