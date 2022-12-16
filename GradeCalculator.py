5e47c81e71ba72ec879b66fd422325d7eff190f5
Michael Totaro <mtotaro@ncsu.edu>
2022-12-16 16:37:41 -0500

    Adding files

---
+++ GradeCalculator.py
@@ -0,0 +1,25 @@
+"""
+This is a grade calculator you can use to calculator your grades
+Change grades and percentages for each class
+"""
+
+""" CSC 231 """
+
+quizzes = 94.01
+labs = 98.71
+assignments = 95
+test1 = 93
+test2 = 97
+final = 90
+
+
+total = (
+      (quizzes * 0.15)
+    + (labs * 0.10)
+    + (assignments * 0.15)
+    + (test1 * 0.20)
+    + (test2 * 0.20)
+    + (final * 0.20)
+)
+
+#print(f"CSC 231: {total}")
